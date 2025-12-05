d61985b5549249cc10e7bb229a8f40120f39dba7
HetshiDekiwadia <dekiwadiahetshi@gmail.com>
2025-12-05 22:31:54 +0530

    added confirm  password validation and toggel icon

--- Register.aspx.cs
+++ Register.aspx.cs
@@ -1,29 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Drawing; // Don't forget this for Color
 
 namespace ASPWeBSM
 {
     public partial class Register : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            UiHelper.ShowSessionToast(this);
-        }
+        // ... (Page_Load remains the same)
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            // --- 1. NEW: Password Match Validation ---
+            if (txtPassword.Text != txtConfirmPassword.Text)
+            {
+                lblMessage.Text = "Passwords do not match. Please enter the same password twice.";
+                lblMessage.ForeColor = Color.Red;
+                return; // Stop execution if passwords don't match
+            }
+
+            // Clear any previous error message before proceeding
+            lblMessage.Text = string.Empty;
+
             string username = txtUser.Text;
             string email = txtEmail.Text;
-            string password = txtPassword.Text;
+            string password = txtPassword.Text; // Use the value from the first box
 
             using (var conn = DatabaseManager.GetConnection())
             {
+                // ... (Rest of your database logic is here)
                 conn.Open();
 
                 using (var cmd = new SqlCommand("Users_CRUD", conn))
@@ -39,8 +44,9 @@ namespace ASPWeBSM
                     object newId = cmd.ExecuteScalar();
                 }
             }
-            UiHelper.SetToast("Registration successful. You can now log in.", "success");
-            // Optional but nice: redirect to login after successful registration
+
+            // Optional but nice: show success message before redirect
+            // Since we passed the password check, we can safely redirect
             Response.Redirect("Login.aspx");
         }
     }
