811cc84741bf5ba6c58d66b11f73f9e3837aff41
Maharshi <maharsh10302@gmail.com>
2025-12-28 16:25:44 +0530

    added valid explaination comments to AuthorizedPage.cs

--- Helper Classes/AuthorizedPage.cs
+++ Helper Classes/AuthorizedPage.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace ASPWeBSM
 {
+    // Instead of adding auth to every single file we inherit all the files to this class
+    // in simple words if youre logged in it lets you load the page..
+    // if not youre redirected to Login.aspx
     public class AuthorizedPage : System.Web.UI.Page
     {
         protected override void OnLoad(EventArgs e)
